0cd12d2093c12913d18637b958e0fc2c79f44892
AubBantolo <jhuliet14@gmail.com>
2023-08-24 15:31:36 +0800

    feat: added anotation for mysql

--- src/main/java/com/afs/restapi/entity/Employee.java
+++ src/main/java/com/afs/restapi/entity/Employee.java
@@ -1,7 +1,5 @@
 package com.afs.restapi.entity;
 
-import net.bytebuddy.asm.Advice;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
