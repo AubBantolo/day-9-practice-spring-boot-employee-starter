8abf56df04e9130f6ab7163db9ab2cc8d3e11928
AubBantolo <jhuliet14@gmail.com>
2023-08-24 17:16:42 +0800

    feat: created companyJpaRepository

--- src/main/java/com/afs/restapi/repository/CompanyJpaRepository.java
+++ src/main/java/com/afs/restapi/repository/CompanyJpaRepository.java
@@ -1,2 +1,9 @@
-package com.afs.restapi.repository;public interface CompanyJpaRepository {
+package com.afs.restapi.repository;
+
+import com.afs.restapi.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyJpaRepository extends JpaRepository<Company, Long> {
 }
