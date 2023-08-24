e968b14d23e36193670e66dd118a3799c250d3c1
AubBantolo <jhuliet14@gmail.com>
2023-08-24 14:24:22 +0800

    feat: created EmployeeJpaRepository

--- src/main/java/com/afs/restapi/entity/Employee.java
+++ src/main/java/com/afs/restapi/entity/Employee.java
@@ -1,7 +1,17 @@
 package com.afs.restapi.entity;
 
+import net.bytebuddy.asm.Advice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer age;
