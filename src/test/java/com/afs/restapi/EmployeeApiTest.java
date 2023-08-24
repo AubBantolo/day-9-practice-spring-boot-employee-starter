045ff911d5da44f8345dece91e2bf8dce9fb772f
AubBantolo <jhuliet14@gmail.com>
2023-08-24 15:42:59 +0800

    test: updated should_find_employee_by_id()

--- src/test/java/com/afs/restapi/EmployeeApiTest.java
+++ src/test/java/com/afs/restapi/EmployeeApiTest.java
@@ -91,11 +91,11 @@ class EmployeeApiTest {
     @Test
     void should_find_employee_by_id() throws Exception {
         Employee employee = getEmployeeBob();
-        employeeJpaRepository.save(employee);
+        Employee savedEmployee = employeeJpaRepository.save(employee);
 
-        mockMvc.perform(get("/employees/{id}", 1))
+        mockMvc.perform(get("/employees/{id}", savedEmployee.getId()))
                 .andExpect(MockMvcResultMatchers.status().is(200))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(savedEmployee.getId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(employee.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(employee.getAge()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.gender").value(employee.getGender()))
