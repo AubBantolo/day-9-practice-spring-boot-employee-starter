e126cff8181ce51237c153265e66459ed5376097
AubBantolo <jhuliet14@gmail.com>
2023-08-24 18:23:49 +0800

    feat: created exceptions

---
+++ src/main/java/com/afs/restapi/exception/EmployeeIsInactiveException.java
@@ -0,0 +1,12 @@
+package com.afs.restapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeIsInactiveException extends RuntimeException {
+
+    public EmployeeIsInactiveException() {
+        super("Employee is inactive");
+    }
+}
